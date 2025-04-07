d5b1931b1cf4897d83cce30f46920c660eea1f99
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-04-07 17:40:48 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
-// Created on: Sep 2020
+// Updated by Lynn Lukose
+// Created on: Mar 20, 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a trapezoid.
+ */
+function calculateAreaOfTrapezoid() {
+  // input
+  const aBaseOfTrapezoid = parseInt(
+    document.getElementById("a-base-of-trapezoid").value
+  )
+    const bBaseOfTrapezoid = parseInt(
+    document.getElementById("b-base-of-trapezoid").value
+  )
+  const heightOfTrapezoid = parseInt(
+    document.getElementById("height-of-trapezoid").value
+  )
+
+  // process
+  const areaOfTrapezoid = [(aBaseOfTrapezoid + bBaseOfTrapezoid) / 2] * heightOfTrapezoid
+
+  // output
+  document.getElementById("area").innerHTML =
+    "The area is: " + areaOfTrapezoid + " mm²"
+}
